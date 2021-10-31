fee788b10e098474a70ce94fa4aede468b18c7d5
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-10-31 18:50:25 -0500

    Users can create playlists

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 client = MongoClient()
@@ -7,16 +7,46 @@ playlists = db.playlists
 
 app = Flask(__name__)
 
-#playlists = [
-#    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-#    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' },
-#    { 'title': 'Code SOS', 'description': 'Programming tips and tricks' }
-#]
+# helper function to create video URLs
+def video_url_creator(id_lst):
+    videos = []
+    for vid_id in id_lst:
+        video = 'https://youtube.com/embed/' + vid_id
+        videos.append(video)
+    return videos
+
 
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists.find())
+
+
+# NEW PLAYLIST
+@app.route('/playlists/new')
+def playlists_new():
+    """Create a new playlist."""
+    return render_template('playlists_new.html')
+
+
+# Note the methods parameter that explicitly tells the route that this is a POST
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+
+    """Submit a new playlist."""
+    # Grab the video IDs and make a list out of them
+    video_ids = request.form.get('video_ids').split()
+    # call our helper function to create the list of links
+    videos = video_url_creator(video_ids)
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
+
 
 
 if __name__ == '__main__':
