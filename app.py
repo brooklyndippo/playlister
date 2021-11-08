4959a436ea4a03b67283df5cd4752313d39226c6
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-11-08 07:47:13 -0600

    users can see single playlists

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask.templating import render_template
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 client = MongoClient()
 db = client.Playlister
@@ -27,9 +29,10 @@ def playlists_index():
 def playlists_new():
     """Create a new playlist."""
     return render_template('playlists_new.html')
+    
 
 
-# Note the methods parameter that explicitly tells the route that this is a POST
+# CREATE A PLAYLIST
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
     """Submit a new playlist."""
@@ -44,7 +47,18 @@ def playlists_submit():
         'video_ids': video_ids
     }
     playlists.insert_one(playlist)
-    return redirect(url_for('playlists_index'))
+    #upddate redirect to the new playlist
+    return render_template('playlists_show.html', playlist=playlist)
+    #DELTE LATER!!! return redirect(url_for('playlists_index'))
+
+#SEE A PLAYLIST
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
+
+
 
 
 if __name__ == '__main__':
